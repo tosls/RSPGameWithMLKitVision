13af7f4d8dad4b6fcb00ea489bcde9f9cba69c04
Anton Bobryshev <65329095+tosls@users.noreply.github.com>
2021-09-01 13:34:02 +0700

    Initial Commit

---
+++ RSPGameWithMLKitVision/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RSPGameWithMLKitVision
+//
+//  Created by Антон Бобрышев on 01.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
